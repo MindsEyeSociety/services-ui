e6416e5378518d99117d0abb6301bd3b219bb963
Joe Terranova <joeterranova@gmail.com>
2017-03-07 11:07:09 -0500

    removed token parsing from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,8 @@
 import { Component } from '@angular/core';
-import {Location} from '@angular/common';
-import { AuthService } from 'app/core/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(private _location: Location,public authService: AuthService){
-    var params = this.decodeQueryString(this._location.path());
-    console.log(params);
-    if(params['token']){
-      console.log('setting token in appComponent',params['token']);
-      this.authService.setToken(params['token']);
-    }
-  }
-  decodeQueryString(path:string){
-    var querystring = path.substring(path.indexOf('?')+1).split('&');
-    var params = {}, pair, d = decodeURIComponent;
-    // march and parse
-    for (var i = querystring.length - 1; i >= 0; i--) {
-      pair = querystring[i].split('=');
-      params[d(pair[0])] = d(pair[1] || '');
-    }
-    return params;
-  }
+  
 }
